75cc340b78b15df57be5e60669292f71a23955c7
Atefalhaj <atefalhaj712@gmail.com>
2025-09-27 14:25:33 -0400

    test

--- OwlHacksP.java
+++ OwlHacksP.java
@@ -1,6 +1,26 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
 public class OwlHacksP {
-  public static void main(String[] args) {
-      System.out.println("Hello World");
-  }  
-}
+    public static void main(String[] args) throws IOException {
+        // Start server on port 8080
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        // Create API endpoint at /hello
+        server.createContext("/hello", (HttpExchange exchange) -> {
+            String response = "Hello from Java backend 👋";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        // Start server
+        server.start();
+        System.out.println("Server running at http://localhost:8080/hello");
+    }
+  }
+
